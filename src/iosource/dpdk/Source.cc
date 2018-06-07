ac3db5305f73e4c32dbc37adc69962f905bf7346
root <miguel.angelcg1996@gmail.com>
2018-06-07 17:57:18 +0200

    deleted deprecated comments

--- src/iosource/dpdk/Source.cc
+++ src/iosource/dpdk/Source.cc
@@ -29,7 +29,7 @@ void DpdkSource::Open(){
 	/* It will be always opened, just need to configure it */
 	if(Configure()){
 		props.is_live = true;
-		props.link_type = 1; // FIXME This one is harcoded, not sure if it affects bro
+		props.link_type = 1; // FIXME This one is harcoded (LINKTYPE_ETHERNET), it probably affects bro but I didn't found a way to make this detection automatic
 		Opened(props);
 
 	}
@@ -65,7 +65,7 @@ void DpdkSource::Statistics(Stats* s){
 		// TODO check/implement rte_eth_stats. Not available for every NIC
 		rte_eth_stats_get(port, &dpdk_stats);
 		if(dpdk_stats.ipackets == 0 && stats.received > 0){
-			printf("Cannot get stats directly from DPDK\n");
+			fprintf(stderr, "Cannot get stats directly from DPDK\n");
 		}
 
 		s->received = stats.received;
@@ -105,28 +105,6 @@ void  DpdkSource::ConvertToPacket(struct rte_mbuf* buf, Packet* pkt){
 	pkt_timeval ts = {current_time(true), 0};
 	u_char* data = rte_pktmbuf_mtod(buf, u_char*);
 		
-	/**
-	 * Initialize from packet data.
-	 *
-	 * @param link_type The link type in the form of a \c DLT_* constant.
-	 *
-	 * @param ts The timestamp associated with the packet.
-	 *
-	 * @param caplen The number of bytes valid in *data*.
-	 *
-	 * @param len The wire length of the packet, which must be more or
-	 * equal *caplen* (but can't be less).
-	 *
-	 * @param data A pointer to the raw packet data, starting with the
-	 * layer 2 header. The pointer must remain valid for the lifetime of
-	 * the Packet instance, unless *copy* is true.
-	 *
-	 * @param copy If true, the constructor will make an internal copy of
-	 * *data*, so that the caller can release its version.
-	 *
-	 * @param tag A textual tag to associate with the packet for
-	 * differentiating the input streams.
-	 */
 	pkt->Init(props.link_type, &ts, rte_pktmbuf_data_len(buf), rte_pktmbuf_pkt_len(buf), data);
 }
 
